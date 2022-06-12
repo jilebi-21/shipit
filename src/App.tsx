4fb1c3880cd7f4ceef347dcbab418d3afe78598c
pruthvi-21 <pruthvishankar397@gmail.com>
2022-06-12 14:35:59 +0530

    Implemented React router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Footer from "./components/Footer";
 import WelcomePage from "./routes/welcome/Welcome";
 
+const ROOT_PATH = "/";
+
 function App() {
 	return (
 		<div className="app-container">
-			<WelcomePage />
+			<Routes>
+				<Route path="/" element={<WelcomePage />} />
+			</Routes>
 			<Footer />
 		</div>
 	);
