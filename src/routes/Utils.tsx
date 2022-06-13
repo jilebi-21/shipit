fa11ef2494ebaf7c971dda66f4bcb4d3d7befd5c
pruthvi-21 <pruthvishankar397@gmail.com>
2022-06-14 00:06:08 +0530

    Added Restaurants api and displayed the names

--- src/routes/Utils.tsx
+++ src/routes/Utils.tsx
@@ -2,3 +2,7 @@ export const Paths = {
 	WELCOME: "/",
 	HOME: "/home",
 };
+
+export const API = {
+	RESTAURANTS: "https://raw.githubusercontent.com/pruthvi-21/Swiggy-Api/master/restaurants.json"
+}
--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
 import Appbar from "../../components/Appbar";
+import { RestaurantBase } from "../../models/RestaurantBase";
+import { API } from "../Utils";
 
 const Home = () => {
+	const [restaurantsList, setRestaurants] = useState<RestaurantBase[]>([]);
+
+	const fetchRestaurants = async () => {
+		const response = await fetch(API.RESTAURANTS);
+		const res = await response.json();
+		setRestaurants(res.response);
+	};
+
+	fetchRestaurants();
+
 	return (
 		<div>
 			<Appbar />
+
+			{restaurantsList.map((item, idx) => {
+				return <div>{item.res_name}</div>;
+			})}
 		</div>
 	);
 };
