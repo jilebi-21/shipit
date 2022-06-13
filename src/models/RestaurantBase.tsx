2b0abbe1710f30762908062260672dae7bb3672f
pruthvi-21 <pruthvishankar397@gmail.com>
2022-06-13 23:53:29 +0530

    Added a Model class for Restaurant

---
+++ src/models/RestaurantBase.tsx
@@ -0,0 +1,10 @@
+export interface RestaurantBase {
+	res_id: number; //Restaurant ID
+	res_name: string; //Restautant Name
+	res_sumary: string; //Restaurant Summary
+	res_rating: number; //Restaurant Ratings
+	res_totalRatings: number; //Total ratings count of a restaurant
+	res_deliveryTime: number; //Delivery time by the restaurant
+	res_costForTwo: number; //Cost at this restaurant for two people
+	res_img: string; //Restaurant preview image
+}
