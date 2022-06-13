17c2e7e042b114ca40921ea1d3faf596da62762c
pruthvi-21 <pruthvishankar397@gmail.com>
2022-06-14 00:20:53 +0530

    Fixed nested styled components continuous rendereing issue
    Removed all the nested styled components in NavBar and moved them to NavBar root

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -1,9 +1,8 @@
-import { Badge, NavbarBrand } from "reactstrap";
+import { NavbarBrand } from "reactstrap";
 import styled from "styled-components";
 import appLogo from "../resources/app-icon.svg";
 import searchIcon from "../resources/navbar-search-icon.svg";
 import profileIcon from "../resources/navbar-profile-icon.svg";
-import cartIconBg from "../resources/navbar-cart-bg.svg";
 import "./test.scss";
 
 const Wrapper = styled.div`
@@ -40,140 +39,120 @@ const Container = styled.div`
 	}
 `;
 
+const LocationContainer = styled.div`
+	display: inline-block;
+	span {
+		font-size: 15px;
+	}
+`;
+
+const SearchIconContainer = styled.div`
+	display: flex;
+	place-items: center;
+	img {
+		width: 17px;
+		aspect-ratio: 1;
+	}
+	p {
+		margin: 0;
+		margin-left: 8px;
+		display: none;
+		@media (min-width: 768px) {
+			display: unset;
+		}
+	}
+`;
+
+const CartIconContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	text-align: center;
+
+	p {
+		margin: 2px;
+	}
+
+	.label {
+		display: none;
+	}
+
+	.counter-container {
+		width: 20px;
+		height: 20px;
+		margin-top: -5px;
+
+		p {
+			position: relative;
+			top: -110%;
+			font-size: 13px;
+		}
+	}
+
+	@media (min-width: 768px) {
+		flex-direction: row;
+
+		.label {
+			display: unset;
+		}
+	}
+`;
+
+const ProfileIconContainer = styled.div`
+	display: flex;
+
+	img {
+		width: 20px;
+		aspect-ratio: 1;
+	}
+	p {
+		margin: 0;
+		margin-left: 8px;
+		display: none;
+		@media (min-width: 768px) {
+			display: unset;
+		}
+	}
+`;
+
+const ColoredSvg = styled.svg`
+	stroke-width: 3px;
+	stroke: #000;
+`;
+
 const Appbar = () => {
-	const LocationView = () => {
-		const Container = styled.div`
-			display: inline-block;
-			span {
-				font-size: 15px;
-			}
-		`;
-
-		return (
-			<Container>
-				<span>Visakhapatnam</span>
-			</Container>
-		);
-	};
-
-	const SearchIcon = () => {
-		const IconContainer = styled.div`
-			display: flex;
-			place-items: center;
-			img {
-				width: 17px;
-				aspect-ratio: 1;
-			}
-			p {
-				margin: 0;
-				margin-left: 8px;
-				display: none;
-				@media (min-width: 768px) {
-					display: unset;
-				}
-			}
-		`;
-		return (
-			<IconContainer>
-				<img src={searchIcon} />
-				<p>Search</p>
-			</IconContainer>
-		);
-	};
-
-	const CartIcon = () => {
-		const CartIndicator = styled.div`
-			display: flex;
-			flex-direction: column;
-			align-items: center;
-
-			p {
-				text-align: center;
-				margin: 2px;
-			}
-
-			.label {
-				display: none;
-			}
-
-			.counter-container {
-				width: 20px;
-				height: 20px;
-				margin-top: -5px;
-
-				p {
-					position: relative;
-					top: -110%;
-					font-size: 13px;
-				}
-			}
-
-			@media (min-width: 768px) {
-				flex-direction: row;
-
-				.label {
-					display: unset;
-				}
-			}
-		`;
-
-		return (
-			<div>
-				<CartIndicator>
-					<div className="counter-container">
-						<Icon />
-						<p>0</p>
-					</div>
-					<p className="label">Cart</p>
-				</CartIndicator>
-			</div>
-		);
-	};
-
-	const Profile = () => {
-		const IconContainer = styled.div`
-			display: flex;
-			place-items: center;
-
-			img {
-				width: 20px;
-				aspect-ratio: 1;
-			}
-			p {
-				margin: 0;
-				margin-left: 8px;
-				display: none;
-				@media (min-width: 768px) {
-					display: unset;
-				}
-			}
-		`;
-		return (
-			<IconContainer>
-				<img src={profileIcon} />
-				<p>Pruthvi</p>
-			</IconContainer>
-		);
-	};
-
 	return (
 		<Wrapper>
 			<Container>
 				<NavbarBrand>
 					<img src={appLogo} width={34} />
 				</NavbarBrand>
-				<LocationView />
+				<LocationContainer>
+					<span>Visakhapatnam</span>
+				</LocationContainer>
 				<div style={{ flexGrow: 1 }}></div>
 
 				<ul>
 					<li>
-						<SearchIcon />
+						<SearchIconContainer>
+							<img src={searchIcon} />
+							<p>Search</p>
+						</SearchIconContainer>
 					</li>
 					<li>
-						<CartIcon />
+						<CartIconContainer>
+							<div className="counter-container">
+								<Icon />
+								<p>0</p>
+							</div>
+							<p className="label">Cart</p>
+						</CartIconContainer>
 					</li>
 					<li>
-						<Profile />
+						<ProfileIconContainer>
+							<img src={profileIcon} />
+							<p>Pruthvi</p>
+						</ProfileIconContainer>
 					</li>
 				</ul>
 			</Container>
@@ -182,16 +161,10 @@ const Appbar = () => {
 };
 
 const Icon = () => {
-	const Colored = styled.svg`
-		fill: #fff;
-		stroke-width: 3px;
-		stroke: #000;
-	`;
-
 	return (
-		<Colored viewBox="-1 0 37 32" height="20" width="20" fill="#fff0">
+		<ColoredSvg viewBox="-1 0 37 32" height="20" width="20" fill="#fff0">
 			<path d="M4.438 0l-2.598 5.11-1.84 26.124h34.909l-1.906-26.124-2.597-5.11z"></path>
-		</Colored>
+		</ColoredSvg>
 	);
 };
 
